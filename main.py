69a75901ed29616c39854640503564e69a5df04a
Rahul <patel.rahul572@hotmail.com>
2025-10-26 21:25:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
-Read the CSV and then calculate the percentage contribution of each product to the total revenue in the Orders.csv.
-	Revenue = Quantity*Price.. 
-	Write into aws db/path
+2. write a function that will accept two strings and return Boolean if they are anagram
+ str1 = rail safety
+ str2= fairy tales
+
+
+def check_ang(str1, str2):
+    A = sorted([x.lower() for x in str1 if x != ' '])
+    B = sorted([x.lower() for x in str2 if x != ' '])
+    return A == B
+
+Write a SQL Query for the below output
+ Input:
+ emp_id name salary dept_id 
+ ----------- ----------- ------------ -----------
+ 1 Alice 50000.00 1
+ 2 Bob 60000.00 2
+ 3 Charlie 50000.00 2
+ 4 Charlie 65000.00 2
+ 5 Eve 45000.00 1
  
-   
-  +-----+-------+-----+-------+
-  |Order_id|Product|Quantity| Price |
-  +-----+-------+-----+-------+
-  | 1001|  A    |   10  |  5.99 |
-  | 1002|  B    |   15  |  8.49 |
-  | 1003|  A    |   20  |  7.99 |
-  | 1004|  C    |   5   |  12.99|
-  | 1005|  B    |   12  |  9.99 |
-  | 1006|  D    |   8   |  6.49 |
-  | 1007|  A    |   25  |  7.49 |
-  | 1008|  C    |   18  |  11.99|
-  +-----+-------+-----+-------+`
+ dept_id dept_name 
+ ----------- ------------
+ 1 HR 
+ 2 Engineering 
+ 3 Sales 
+ 
+ Output:
+ dept_name employee_count highest_salary
+ --------------- -------------- --------------
+ Engineering 3 65000
+ HR 2 50000
+ Sales 0 0
 
 
-sol 1:
-with main as (
-
-select product, sum(quantity*price) as prod_rev from orders_tbl
-group by product),
-
-sub as (
-select *,sum(prod_rev) over (order by (select null)) as basser from main)
-
-select *,(prod_rev/basser * 100) as product_contribution from sub
-
-
-
-sol 2: 
-
-with main as (
-
-select product, sum(quantity*price) as prod_rev from orders_tbl
-group by product),
-
-sub as (
-select sum(prod_rev) as basser from main)
-
-select *,(prod_rev/basser * 100) as product_contribution  from main cross join sub
-
-
-
-
+SQL problem - 
+solve in sql - You want to identify the hierarchy of employees based on the managerid.
+employee 
+id name managerid 
+1 abc null
+2 xyz 1
+3 jhl 2
+4 hfg 2 
+if given id = 3 then output should be - 
+abc 
+xyz
+jhl"
